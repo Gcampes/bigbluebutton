0cc33dc99be5d034f3fab9d73837d187837de0cc
Gabriel Carvalho de Campes <gabrieldecampes@gmail.com>
2016-11-16 08:41:53 -0200

    fix clear poll imports

--- bigbluebutton-html5/imports/api/meetings/server/modifiers/clearMeetings.js
+++ bigbluebutton-html5/imports/api/meetings/server/modifiers/clearMeetings.js
@@ -6,7 +6,7 @@ import { clearUsersCollection } from '/imports/api/users/server/modifiers/clearU
 import clearChats from '/imports/api/chat/server/modifiers/clearChats';
 import { clearShapesCollection } from '/imports/api/shapes/server/modifiers/clearShapesCollection';
 import { clearSlidesCollection } from '/imports/api/slides/server/modifiers/clearSlidesCollection';
-import clearPoll from '/imports/api/polls/server/modifiers/clearPoll';
+import clearPolls from '/imports/api/polls/server/modifiers/clearPolls';
 import { clearCursorCollection } from '/imports/api/cursor/server/modifiers/clearCursorCollection';
 import { clearCaptionsCollection }
   from '/imports/api/captions/server/modifiers/clearCaptionsCollection';
@@ -18,7 +18,7 @@ export default function clearMeetings() {
     clearCaptionsCollection();
     clearChats();
     clearCursorCollection();
-    clearPoll();
+    clearPolls();
     clearPresentationsCollection();
     clearShapesCollection();
     clearSlidesCollection();
--- bigbluebutton-html5/imports/api/meetings/server/modifiers/removeMeeting.js
+++ bigbluebutton-html5/imports/api/meetings/server/modifiers/removeMeeting.js
@@ -6,7 +6,7 @@ import { clearUsersCollection } from '/imports/api/users/server/modifiers/clearU
 import clearChats from '/imports/api/chat/server/modifiers/clearChats';
 import { clearShapesCollection } from '/imports/api/shapes/server/modifiers/clearShapesCollection';
 import { clearSlidesCollection } from '/imports/api/slides/server/modifiers/clearSlidesCollection';
-import clearPoll from '/imports/api/polls/server/modifiers/clearPoll';
+import clearPolls from '/imports/api/polls/server/modifiers/clearPolls';
 import { clearCursorCollection } from '/imports/api/cursor/server/modifiers/clearCursorCollection';
 import { clearCaptionsCollection }
   from '/imports/api/captions/server/modifiers/clearCaptionsCollection';
@@ -29,7 +29,7 @@ export default function removeMeeting(meetingId) {
       clearCaptionsCollection(meetingId);
       clearChats(meetingId);
       clearCursorCollection(meetingId);
-      clearPoll(meetingId);
+      clearPolls(meetingId);
       clearPresentationsCollection(meetingId);
       clearShapesCollection(meetingId);
       clearSlidesCollection(meetingId);
